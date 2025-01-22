259562e004b0a2e4d66c8044f662fe70e1f43758
Eliseu costa <eliseualbano57@gmail.com>
2025-01-22 14:24:38 -0300

    criando o slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './assets/componetes/Navbar/NavBar'
+// import Hero from './assets/componetes/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      {/* <Hero /> */}
     </div>
   )
 }
