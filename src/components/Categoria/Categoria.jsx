0c42c5b219cb5661e02dbfa7f45f685137112665
Eliseu costa <eliseualbano57@gmail.com>
2025-01-27 14:24:26 -0300

    criando o primero card

--- src/components/Categoria/Categoria.jsx
+++ src/components/Categoria/Categoria.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Button from "../Shared/Button";
+import Image1 from "../../assets/Categoria/earphone.png";
+import Image2 from "../../assets/Categoria/watch.png";
+import Image3 from "../../assets/Categoria/macbook.png";
 
 const Categoria = () => {
   return (
-    <div>
-      Categoria
+    <div className="py-8">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 ">
+          {/* primeira coluna */}
+          <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70
+           text-white rounded-3xl relative flex items-end h-[320px]">
+            <div>
+              <div className="mb-4">
+                <p className="mb-[2px] text-gray-400">Aproveite</p>
+                <p className="text-2xl font-semibold mb-[2px]">Com</p>
+                <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-4">Fones de ouvidos</p>
+                <Button 
+                  text="Comprar"
+                  bgColor={"bg-principal"}
+                  textColor={"text-white"}
+                />
+              </div>
+            </div>
+            <img src= {Image1} alt="" className="w-[320px] absolute bottom-0"/>
+          </div>
+          {/* segunda coluna */}
+          <div></div>
+          {/* terceira coluna */}
+          <div></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Categoria
+export default Categoria;
