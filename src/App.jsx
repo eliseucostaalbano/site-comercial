61ceb243ba74fad408c8f82e5693792ead6b26f0
Eliseu costa <eliseualbano57@gmail.com>
2025-01-27 13:21:26 -0300

    criando o componente categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import NavBar from './components/Navbar/NavBar'
-import Hero from './components/Hero/Hero'
+import React from 'react';
+import NavBar from './components/Navbar/NavBar';
+import Hero from './components/Hero/Hero';
+import Categoria from './components/Categoria/Categoria';
 
 const App = () => {
   return (
     <div>
       <NavBar/>
       <Hero />
+      <Categoria />
     </div>
   )
 }
---
+++ src/components/Categoria/Categoria.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categoria = () => {
+  return (
+    <div>
+      Categoria
+    </div>
+  )
+}
+
+export default Categoria
