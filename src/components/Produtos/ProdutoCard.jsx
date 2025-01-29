4b262c3931b132f7980f5f930f083d16b0ab46be
Eliseu costa <eliseualbano57@gmail.com>
2025-01-29 16:50:16 -0300

    Criando o componente Produtos e ProdutoCard

---
+++ src/components/Produtos/ProdutoCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProdutoCard = ({data}) => {
+  return (
+    <div>
+      ProdutoCard
+    </div>
+  )
+}
+
+export default ProdutoCard
--- src/components/Produtos/Produtos.jsx
+++ src/components/Produtos/Produtos.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import Heading from '../Shared/Heading'
+import ProdutoCard from './ProdutoCard'
+
+// import de imagens
+import Image1 from "../../assets/Produtos/p-1.jpg";
+import Image2 from "../../assets/Produtos/p-2.jpg";
+import Image3 from "../../assets/Produtos/p-3.jpg";
+import Image4 from "../../assets/Produtos/p-4.jpg";
+
+const ProdutoData = [
+  {
+    id: 1,
+    img: Image1,
+    titulo: "Headphone de Barco",
+    preço: "120",
+    AosDelay: "0",
+  },
+  {
+    id: 2,
+    img: Image2,
+    titulo: "Montanha Rochosa",
+    preço: "420",
+    AosDelay: "200",
+  },
+  {
+    id: 3,
+    img: Image3,
+    titulo: "Óculos",
+    preço: "320",
+    AosDelay: "400",
+  },
+  {
+    id: 4,
+    img: Image4,
+    titulo: "Impresso",
+    preço: "220",
+    AosDelay: "600",
+  },
+];
 
 const Produtos = () => {
   return (
     <div>
-      Produtos
+    <div className='container'>
+      {/* seção Header */}
+    <Heading titulo="Nossos Produtos"
+       subtitulo="Explore Nossos Produtos"/>
+      {/* seção Body */}
+     <ProdutoCard data={ProdutoData}/>
+    </div>
     </div>
   )
 }
