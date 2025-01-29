230bf0ab0eb7baa44eb4cc1544be5e211fe83cef
Eliseu costa <eliseualbano57@gmail.com>
2025-01-29 14:34:04 -0300

    criando o componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,34 @@ import Hero from './components/Hero/Hero';
 import Categoria from './components/Categoria/Categoria';
 import Categoria2 from './components/Categoria/Categoria2';
 import Serviços from './components/Serviços/Serviços'
+import Banner from './components/Banner/Banner'
+import Produtos from "./components/Produtos/Produtos";
+import Footer from "./components/Footer/Footer"
+
+import headphone from "./assets/Hero/headphone.png";
+import smartwatch2 from "./assets/Categoria/smartwatch2.png";
+
+const BannerData = {
+  desconto: "30% OFF",
+  titulo: "Belo Sorriso",
+  date: "10 Jan até 28 Jan",
+  image: headphone,
+  titulo2: "Air Solo Bass",
+  titulo3: "Liquidação de inverno",
+  titulo4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
+
+const BannerData2 = {
+  desconto: "30% OFF",
+  titulo: "Horas felizes",
+  date: "14 Jan até 28 Jan",
+  image: smartwatch2,
+  titulo2: "Smart Solo",
+  titulo3: "Liquidação de inverno",
+  titulo4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
 
 const App = () => {
   return (
@@ -13,6 +41,10 @@ const App = () => {
       <Categoria />
       <Categoria2 />
       <Serviços />
+      <Banner data={BannerData} />
+      <Produtos />
+      <Banner data={BannerData2} />
+      <Footer />
     </div>
   )
 }
